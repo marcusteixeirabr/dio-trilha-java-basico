f56d7d8d65d1b2c8f9538f0183075d208b75eea1
marcusteixeirabr <marcusteixeirabr@gmail.com>
2025-10-10 20:57:49 -0300

    Introdução dos comentários na classe Desafio

--- DesafioCodigo/src/Desafio.java
+++ DesafioCodigo/src/Desafio.java
@@ -2,38 +2,62 @@ import java.util.Scanner;
 
 public class Desafio {
     public static void main(String[] args) {
-        float salary, benefits, tax, netSalary;
+
+        // Define variáveis
+        float salary, benefits, taxRate, netSalary;
+
+        // Método validateFloat recebe a pergunta e retorna um valor float válido
         salary = validateFloat("Digite o valor do salário: R$ ");
         benefits = validateFloat("Digite o valor dos benefícios: R$ ");
-        tax = calculateTax(salary);
-        netSalary = salary - salary * tax + benefits;
+
+        // Método calculateTaxRate recebe o valor do salário e retorna o valor da taxa de imposto
+        taxRate = calculateTaxRate(salary);
+
+        // Cálculo do salário líquido
+        netSalary = salary - salary * taxRate + benefits;
+
+        // saída do sistema
         System.out.printf("O salário final é igual a R$ %.2f", netSalary);
     }
 
     static float validateFloat (String text) {
+
+        // Define variáveis
         String answer;
         float value;
+
+        // Abre entrada do sistema
         Scanner scanner = new Scanner(System.in);
+
+        // Imprime a pergunta recebida
         System.out.print(text);
+
+        // Repete a pergunta atá que seja digitado um valor válido
         do {
+
+            // Trata a entrada do usuário, retirando os espaços em branco das extremidades e troca a "," por "."
             answer = scanner.nextLine().trim().replace(",", ".");
+
+            // Verifica se é possível realizar o casting de string para float
             try {
                 value = Float.parseFloat(answer);
                 break;
             } catch (Exception error) {
+
+                // Se não for possível imprime uma nova solicitação de valor
                 System.out.print("Digite um valor válido: R$ ");
             }
         } while (true);
         return value;
     }
 
-    static float calculateTax(float salary) {
+    static float calculateTaxRate(float salary) {
         float tax = 0.0f;
-        if (salary <= 11000.0) {
+        if (salary <= 1100.0) {
             tax = 0.05f;
-        } else if (salary <= 25000.0) {
+        } else if (salary <= 2500.0) {
             tax = 0.1f;
-        } else if (salary > 25000.0) {
+        } else if (salary > 2500.0) {
             tax = 0.15f;
         }
         return tax;
